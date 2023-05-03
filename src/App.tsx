690a4a5c09a02de8d5fae879140bff1498f82d8a
Edinho Rodrigues <edinho@gebit.io>
2023-05-03 07:55:56 -0300

    componente de Avatar e Post

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
-import {Post } from './components/Post'
+import { Post, PostType } from './components/Post'
 import { Header } from './components/Header'
 import { Sidebar } from './components/Sidebar'
 
 import './global.css'
 import styles from './App.module.css'
 
-const posts = [
+const posts: PostType[] = [
   {
     id: 1,
     author: {
@@ -53,9 +53,7 @@ function App() {
               return (
                 <Post
                   key={post.id}
-                  author={post.author}
-                  content={post.content as []}
-                  publishedAt={post.publishedAt}
+                  post={post}
                 />
               )
             })
